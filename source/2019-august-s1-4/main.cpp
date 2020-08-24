caf603f771f68d15ce7ff96a0b2f297787f2692b
m133solomon <solomonmihai10@gmail.com>
2020-08-24 14:24:41 +0300

    Day #11

---
+++ source/2019-august-s1-4/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----- bac -----\n" << endl;
+
+    /*
+     * numarul de noduri este mai mare cu 1 decat numarul de muchii
+     * => raspuns corect ( D )
+     *
+     *                  (1)
+     *                 /   \
+     *                (2)  (3)
+     *               /   \
+     *             (4)   (5)
+     */
+
+    cout << "\n----- END -----" << endl;
+}
