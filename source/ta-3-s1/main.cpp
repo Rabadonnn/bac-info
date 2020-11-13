ff83af1c0aa7dac668f13227f61262fe73a532ef
m133solomon <solomonmihai10@gmail.com>
2020-11-13 21:16:43 +0200

    Day #38

---
+++ source/ta-3-s1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "----- ta-3-s1 -----\n" << endl;
+
+   /*
+
+      1. d
+      2. b
+      3. b
+      4. grafuri
+      5. abrbori
+
+   */
+
+   cout << "\n----- END -----" << endl;
+}
